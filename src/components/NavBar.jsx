4c6cea6597dfdce5d5de525a905828877f210787
MHMD101 <atrach.muhammad@gmail.com>
2023-07-23 11:04:46 -0400

    Initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+    // javascript here
+  return (
+    <div>
+        <h1>Header and navbar</h1>
+        
+    </div>
+  )
+}
+
+export default NavBar
